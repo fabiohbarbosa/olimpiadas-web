a94ba898426e2fae37fde777e38b6daf7fddd2de
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2016-07-04 22:30:06 -0300

    Health check

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import load from 'express-load';
+import bodyParser from 'body-parser';
 
 import scheduler from './scheduler';
 import {
@@ -11,13 +12,19 @@ log.info("Application starting to profile '" + nodeEnv + "'");
 
 let app = express();
 
-load('controllers')
-  .then('routes')
-  .into(app);
+// json body
+log.debug('Configuring JSON body parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-scheduler.start();
+// load apis
+load('app/apis').into(app);
 
 let port = process.env.PORT || 8080;
 app.listen(port);
 
 log.info('Server started on port ' + port);
+
+scheduler.start();

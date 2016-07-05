797267f0b967877f38bf279d4ee23c09da1e35d0
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2016-07-04 22:41:40 -0300

    Rollback

--- app/server.js
+++ app/server.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import load from 'express-load';
-import bodyParser from 'body-parser';
-
 import scheduler from './scheduler';
 import {
   log,
@@ -11,16 +8,6 @@ import {
 log.info("Application starting to profile '" + nodeEnv + "'");
 
 let app = express();
-
-// json body
-log.debug('Configuring JSON body parser');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-// load apis
-load('app/apis').into(app);
 
 let port = process.env.PORT || 8080;
 app.listen(port);

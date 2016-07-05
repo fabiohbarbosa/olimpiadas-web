b00e66385337adb6d7dc9f48e530d21d4dcf845e
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2016-07-04 22:35:20 -0300

    Health check

---
+++ app/apis/health-check.js
@@ -0,0 +1,9 @@
+import { log } from '../utils';
+
+module.exports = (router) => {
+  log.debug('Health check API loaded');
+
+  router.get('/health-check', (req, res, next) => { // eslint-disable-line no-unused-vars
+    res.status(200).json({'message': 'OK'});
+  });
+};

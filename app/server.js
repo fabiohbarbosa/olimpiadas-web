8dbae99543d71d3545ab9988312eb06a2ca9e182
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2016-07-04 22:58:36 -0300

    Health check

--- app/server.js
+++ app/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import scheduler from './scheduler';
+import glob from 'glob';
+import bodyParser from 'body-parser';
+
 import {
   log,
   nodeEnv
@@ -11,6 +14,20 @@ let app = express();
 
 let port = process.env.PORT || 8080;
 app.listen(port);
+
+// routes and body parser
+// json body
+log.debug('Configuring JSON body parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+log.debug('Creating APIs');
+let router = express.Router();
+let routes = glob.sync(__dirname + '/apis/**/**.js');
+routes.forEach((route) => require(route)(router)); // eslint-disable-line global-require
+app.use('/api', router);
 
 log.info('Server started on port ' + port);
 
